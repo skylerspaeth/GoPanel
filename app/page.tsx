f3e37727a3beaeaafe9eff774de6986efde346c1
Skyler Spaeth <skylerspaeth.scc@me.com>
2024-01-06 15:39:42 -0600

    get gamemode selection modal to a very basic working state that displays ul of modes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { useState } from "react";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Image, useDisclosure } from "@nextui-org/react";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+
+import { GamemodeModal } from "@/components/gamemode-modal";
 
 const maps = [
   {
@@ -42,11 +45,21 @@ const maps = [
 ];
 
 export default function Home() {
+  // destructure and rename keys for gamemode selection modal
+  const {
+    isOpen: gmModalIsOpen,
+    onOpen: openGmModal,
+    onOpenChange: onGmModalChange
+  } = useDisclosure();
+
+  // track supported gamemodes for requested map
+  const [modes, setModes] = useState([]);
+
 	return (
     <>
       <section className="flex flex-col items-center justify-center gap-4 py-2">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title({size: "sm"})}>Welcome to&nbsp;</h1>
+          <h1 className={title({ size: "sm" })}>Welcome to&nbsp;</h1>
           <h1 className={title({ size: "sm", color: "violet" })}>GoPanel</h1>
           <h2 className={subtitle({ class: "mt-4" })}>
             Start by selecting a map to host on the server.
@@ -56,7 +69,7 @@ export default function Home() {
       <section className="py-6">
         <div className="gap-4 grid grid-cols-2 sm:grid-cols-4">
           {maps.map((item, index) => (
-            <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+            <Card shadow="sm" key={index} isPressable onPress={() => { setModes(item.gamemodes); openGmModal() }}>
               <CardBody className="overflow-visible p-0">
                 <Image
                   shadow="sm"
@@ -73,6 +86,7 @@ export default function Home() {
               </CardFooter>
             </Card>
           ))}
+          <GamemodeModal isOpen={gmModalIsOpen} onOpenChange={onGmModalChange} modes={modes} />
         </div>
       </section>
     </>
