cc1220c6bd2bc67ea2eae5b200db031423d2e25e
Skyler Spaeth <skylerspaeth.scc@me.com>
2024-01-06 19:04:03 -0600

    make gamemode modal use pretty radio boxes and go button

--- components/gamemode-modal.tsx
+++ components/gamemode-modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, RadioGroup, Radio, cn } from "@nextui-org/react";
 import { ArrowIcon } from "@/components/icons";
 
 // map short gamemode codes to full-length labels
@@ -17,24 +17,32 @@ const gmCodeMap = {
 
 export const GamemodeModal = ({isOpen, onOpenChange, gamemodes}) => {
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} classNames={{footer: "justify-start"}}>
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} classNames={{header: "pb-2", footer: "justify-start"}}>
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">Select a gamemode to host</ModalHeader>
+            <ModalHeader className="flex flex-col gap-1 text-foreground-500">Select a gamemode to host</ModalHeader>
             <ModalBody>
-              {gamemodes.map((gmCode) =>
-                <div className="flex gap-4 items-center">
-                  <Button endContent={<ArrowIcon />} className="bg-gradient-to-tr from-[#e11cff] to-[#b249f8]">
+              <RadioGroup color="secondary">
+                {gamemodes.map((gmCode) =>
+                  <Radio
+                    value={gmCodeMap[gmCode]}
+                    classNames={{base: cn(
+                      "w-full max-w-full inline-flex m-0 bg-content1 hover:bg-content2 items-center",
+                      "group active:opacity-50 flex-row-reverse justify-between tap-highlight-transparent",
+                      "cursor-pointer border-2 border-default rounded-lg gap-4 p-4",
+                      "data-[selected=true]:border-secondary",
+                    )}}
+                  >
                     {gmCodeMap[gmCode]}
-                  </Button>
-                </div>
-              )}
+                  </Radio>
+                )}
+              </RadioGroup>
             </ModalBody>
             <ModalFooter>
-              <div className="flex gap-4 items-center">
-                <Button onPress={onClose} className="bg-gradient-to-bl from-[#ED213A] to-[#93291E]">
-                  Cancel
+              <div className="flex gap-4 items-center w-full">
+                <Button onPress={onClose} className="text-md w-full bg-gradient-to-tr from-[#006734] to-[#00a24f]">
+                  Go
                 </Button>
               </div>
             </ModalFooter>
